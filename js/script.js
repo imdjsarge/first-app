9ada1d9e0138428db8111d9bbdf6bdc5550180c9
imdjsarge <rgallo001@my.hennepintech.edu>
2015-09-30 20:01:04 -0500

    basic html, css, js started

---
+++ js/script.js
@@ -0,0 +1,44 @@
+(function () {
+
+    var fav_movies = {
+        "movies": [
+            {
+                "name": "Forest Gump",
+                "genre": "Drama",
+        },
+            {
+                "name": "Shawshank Redemption",
+                "genre": "Drama",
+        },
+            {
+                "name": "Avengers",
+                "genre": "Action",
+        },
+            {
+                "name": "Ghostbusters",
+                "genre": "Comedy",
+        },
+            {
+                "name": "Penguins",
+                "genre": "Animated",
+        },
+            {
+                "name": "Evil Dead 2",
+                "genre": "Horror",
+        },
+            {
+                "name": "Step Brothers",
+                "genre": "Comedy",
+        },
+            {
+                "name": "Superman",
+                "genre": "Action",
+        }
+        ]
+    };
+    
+    
+
+
+
+}());
