7f757141ce914c420f682f980ce316a4aff1ecc5
imdjsarge <rgallo001@my.hennepintech.edu>
2015-10-05 06:48:50 -0500

    changes

--- js/script.js
+++ js/script.js
@@ -1,51 +1,51 @@
 (function () {
 
-        var fav_movies = {
-            "movies": [
-                {
-                    "name": "Forest Gump",
-                    "genre": "Drama",
+    var fav_movies = {
+        "movies": [
+            {
+                "name": "Forest Gump",
+                "genre": "Drama",
         },
-                {
-                    "name": "Shawshank Redemption",
-                    "genre": "Drama",
+            {
+                "name": "Shawshank Redemption",
+                "genre": "Drama",
         },
-                {
-                    "name": "Avengers",
-                    "genre": "Action",
+            {
+                "name": "Avengers",
+                "genre": "Action",
         },
-                {
-                    "name": "Ghostbusters",
-                    "genre": "Comedy",
+            {
+                "name": "Ghostbusters",
+                "genre": "Comedy",
         },
-                {
-                    "name": "Penguins",
-                    "genre": "Animated",
+            {
+                "name": "Penguins",
+                "genre": "Animated",
         },
-                {
-                    "name": "Evil Dead 2",
-                    "genre": "Horror",
+            {
+                "name": "Evil Dead 2",
+                "genre": "Horror",
         },
-                {
-                    "name": "Step Brothers",
-                    "genre": "Comedy",
+            {
+                "name": "Step Brothers",
+                "genre": "Comedy",
         },
-                {
-                    "name": "Superman",
-                    "genre": "Action",
+            {
+                "name": "Superman",
+                "genre": "Action",
         }
         ]
-        };
-
-    
-        var object = fav_movies.movies;
-            movie_count = object.length, i;
-            for (i = 0; i < movie_count; i = i + i) {
-    
-
-        },
+    };
 
 
+    var object = fav_movies.movies;
+    movie_count = object.length, i;
+    for (i = 0; i < movie_count; i = i++) {
 
 
-        })();
+    },
+
+
+
+
+})();
