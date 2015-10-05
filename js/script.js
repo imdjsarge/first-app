d4eaa53d0359bbea291a628d97ab8257a8e95a4b
imdjsarge <rgallo001@my.hennepintech.edu>
2015-10-05 06:45:07 -0500

    added images

--- js/script.js
+++ js/script.js
@@ -3,47 +3,61 @@
         var fav_movies = {
             "movies": [
                 {
+                
                     "name": "Forest Gump",
                     "genre": "Drama",
+                    "image": "images/forest_gump.jpg",
         },
                 {
+                
                     "name": "Shawshank Redemption",
                     "genre": "Drama",
+                    "image": "images/shawshank_redemption.jpg",
         },
                 {
+              
                     "name": "Avengers",
                     "genre": "Action",
+                    "image": "images/avengers.jpg",
         },
                 {
+                   
                     "name": "Ghostbusters",
                     "genre": "Comedy",
+                    "image": "images/ghostbusters.jpg",
         },
                 {
+    
                     "name": "Penguins",
                     "genre": "Animated",
+                    "image": "images/penguines.jpg",
         },
                 {
                     "name": "Evil Dead 2",
                     "genre": "Horror",
+                    "image": "images/evel_dead_2.jpg",
         },
                 {
                     "name": "Step Brothers",
                     "genre": "Comedy",
+                    "image": "images/step_brothers.jpg",
         },
                 {
                     "name": "Superman",
                     "genre": "Action",
+                    "image": "images/superman.jpg",
         }
         ]
         };
 
     
-        var object = fav_movies.movies;
-            movie_count = object.length, i;
-            for (i = 0; i < movie_count; i = i + i) {
-    
-
-        },
+        var favorite_movies = fav_movies.name;
+         for (i = 0; i < favorite_movies.length; i++) {
+             my_movies = fav_movies.name[i];
+             
+             
+             
+         },
 
 
 
