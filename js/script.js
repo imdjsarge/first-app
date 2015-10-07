a40a303abf44addab6b6fb6f3e475d04eba3e118
imdjsarge <rgallo001@my.hennepintech.edu>
2015-10-06 21:10:38 -0500

    basic background

--- js/script.js
+++ js/script.js
@@ -59,8 +59,16 @@
         var new_movies = document.createElement("h1");
         var movie = document.createTextNode(my_movies.name);
         movie_names.appendChild(movie);
-        
-  
+
+        line_break = document.getElementById("movies");
+        var linebreak = document.createElement("br");
+        line_break.appendChild(linebreak);
+
+
+        line_break = document.getElementById("movies");
+        var linebreak = document.createElement("br");
+        line_break.appendChild(linebreak);
+
 
 
         movie_img = document.getElementById("movies");
@@ -69,11 +77,26 @@
         movie_img.appendChild(new_images);
 
 
+        line_break = document.getElementById("movies");
+        var linebreak = document.createElement("br");
+        line_break.appendChild(linebreak);
+
+
         movie_genre = document.getElementById("movies");
         var new_genre = document.createElement("h4");
         var genre = document.createTextNode(my_movies.genre);
-        new_genre.appendChild(genre);
-        
+        movie_genre.appendChild(genre);
+
+        line_break = document.getElementById("movies");
+        var linebreak = document.createElement("br");
+        line_break.appendChild(linebreak);
+
+        line_break = document.getElementById("movies");
+        var linebreak = document.createElement("br");
+        line_break.appendChild(linebreak);
+
+
+
 
 
     }
