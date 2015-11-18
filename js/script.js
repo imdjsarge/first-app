627ab3609e0c95d3ed93d6df3a5bf4053175b919
imdjsarge <rgallo001@my.hennepintech.edu>
2015-11-18 07:28:07 -0600

    pics are up, videos not working

--- js/script.js
+++ js/script.js
@@ -10,70 +10,114 @@ $(document).ready(function () {
                 "size": "50px"
             });
             $("<p>Click on the movie to watch the trailer!!!!</p>").insertAfter("h1");
-        }
-    };
-    firstApp.buildHeader();
+        },
 
-    // AJAX -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
-    var movies = {
+
+
+        // AJAX -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+
         movieItems: function (data) {
             $.ajax({
                 type: "get",
                 url: "data/movies.json",
                 dataType: "json",
                 success: function (data) {
-             
+
                     var movies = data.movies,
                         count = movies.length;
                     if (count > 0) {
-                        $.each(movies, function (i, obj) {});
-                        console.log(Object);
+                        firstApp.buildList(movies);
                     }
-            
-                    
+
+
                 },
-                              error: function () {
-                alert('an ajax error occurred');
-            }
-              
-            });
-            
-        }
-    };
-
-    movies.movieItems();
-
-var list = {
-    buildList: function (movies) {
-        var list = movies.buildList,
-            count = list.length;
-        if (count > 0 ) {
-            $.each(list, function (i, obj) {
-                
-                if (movies.obj.type === "Drama") {
-                    var li = $("<li>");
-                    li.append(obj.name + " " + obj.image);
-                    if (ojb.imgage !== undefined) {
-                        li.append("<img src=" + ojb.imgage + ">");
-                        console.log(list);
-                        
-                    }
+                error: function () {
+                    alert('an ajax error occurred');
                 }
+
             });
-        }
+
+        },
+
+
+
+
+
+        buildList: function (movies) {
+
+            var count = movies.length;
+            if (count > 0) {
+                $.each(movies, function (i, obj) {
+
+                    $("ajax");
+                    $("load");
+                    $("save");
+                    $("clear");
+                    
+            
+
+                 list = $("<li>").append(obj.name + "<br></br>" + obj.genre + "<br></br>").insertAfter("#movies");
+                    
+                    
+                    if (obj.image !== undefined) {
+                        list.append("<img src=" + obj.image + ">");
+                    }
+                    
         
-    }
-    
-};
-    list.buildList();
-    
-    buildMovies = {
-        buildMovies: function (data) {
-$("<div></div>").insertAfter("p");
-    $("<li>").insertAfter("div");
-    $("<li>").append(list).insertAfter("ul");         
+       if (obj.video !== undefined) {
+           list.append(obj.video).insertAfter(list);
+       }
+                   
+                    
+
+                    //movie_names = document.getElementById("movies")
+                    //var new_movies = document.createElement("h1");
+                    //var movie = document.createTextNode(movies.name);
+                    //new_movies.appendChild(movie);
+                    //movie_names.appendChild(new_movies);
+
+                    //---------------------------------
+
+                    //movie_img = document.getElementById("movies");
+                    //var new_images = document.createElement("img");
+                    //new_images.setAttribute("src", movies.image);
+                    //movie_img.appendChild(new_images);
+
+                    //-----------------------------------
+                    //movie_genre = document.getElementById("movies");
+                    //var new_genre = document.createElement("h2");
+                    //var genre = document.createTextNode(movies.genre);
+                    //new_genre.appendChild(genre);
+                    //movie_genre.appendChild(new_genre);
+
+                    //--------------------------------------
+
+                    //var youTubeEmbed = "https://www.youtube.com/embed/"
+
+                    //videos = document.getElementById("movies");
+                    //var new_video = document.createElement("iframe");
+                    //videos.setAttribute("frameborder", "0");
+                    //videos.setAttribute("allowfullscreen", "allowfullscreen");
+                    //new_video.setAttribute("src", youTubeEmbed + movies.video);
+                    //videos.appendChild(new_video);
+
+
+                });
             }
+
+        },
+
+
+
+
     };
-    buildMovies.buildMovies();
-    
+
+
+    firstApp.buildHeader();
+
+    firstApp.movieItems();
+
+
+
+
 }());
